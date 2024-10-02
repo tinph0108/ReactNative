4a4f1e284da8e41ff84b1ae2f36792eae38d8cf7
tinph010803 <tinphan924@gmail.com>
2024-10-02 10:56:45 +0700

    Tuan06 ReactNative Add Screen Home App TakeNotes

---
+++ Lab06/design_app_takenote_api/App.js
@@ -0,0 +1,22 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './HomeScreen'; // Màn hình đầu tiên
+import TaskScreen from './TaskScreen'; // Màn hình danh sách nhiệm vụ
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          options={{ headerShown: false }} 
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
