a4907a7ab371fc42153a74c2e4c8f6889e50eb7f
tinph010803 <tinphan924@gmail.com>
2024-10-30 10:05:46 +0700

    Bai Tap Redux Saga

---
+++ redux_saga/screens/HomeScreen.js
@@ -0,0 +1,83 @@
+import React, {useState} from 'react';
+import { SafeAreaView, View, Text, TextInput, TouchableOpacity, StyleSheet,Image } from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
+const HomeScreen = ({ navigation }) => {
+
+  const [name, setName] = useState('');
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image
+        source={{ uri: 'https://i.postimg.cc/T3YrTGCv/f6ee0953600008083c32857b2d79ab5e.png' }}
+        style={styles.logo}
+      />
+      <Text style={styles.title}>MANAGE YOUR TASK</Text>
+      <View style={styles.inputContainer}>
+        <FontAwesome name="envelope" size={20} color="gray" style={styles.icon} />
+        <TextInput
+          style={styles.input}
+          placeholder="Enter your name"
+          placeholderTextColor="gray"
+          value={name}
+          onChangeText={setName}
+  
+        />
+      </View>
+      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Tasks',{userName: name})}>
+        <Text style={styles.buttonText}>GET STARTED →</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f8f8f8',
+  },
+  logo: {
+     width: 300,
+    height: 300,
+    marginBottom: 40
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#7F3DFF',
+    marginBottom: 40,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderWidth: 1, 
+    borderColor: '#e0e0e0',
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    width: '80%',
+    marginBottom: 30,
+  },
+  icon: {
+    marginRight: 10,
+  },
+  input: {
+    fontSize: 16,
+    color: '#000',
+  },
+  button: {
+    backgroundColor: '#00CFFF',
+    paddingVertical: 15,
+    paddingHorizontal: 50,
+    borderRadius: 25,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
+
+export default HomeScreen;
